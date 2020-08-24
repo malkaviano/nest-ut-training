75170e96aae7ea96ce71cf53fd5ed58bda4da466
Rafael Karayannopoulos <rafael.kss@gmail.com>
2020-08-24 18:45:14 -0300

    controller test

--- src/unit-testing/unit-testing.controller.spec.ts
+++ src/unit-testing/unit-testing.controller.spec.ts
@@ -1,18 +1,56 @@
 import { Test, TestingModule } from '@nestjs/testing';
+
 import { UnitTestingController } from './unit-testing.controller';
+import { UnitTestingService } from './unit-testing.service';
+import { BadRequestException } from '@nestjs/common';
+
+const mockApplyRule = jest.fn();
+
+// Jest auto mock feature
+jest.mock('./unit-testing.service', () => {
+    return {
+        UnitTestingService: jest.fn().mockImplementation(
+            () => ({ applyRule: mockApplyRule })
+        ),
+    }
+});
 
 describe('UnitTestingController', () => {
-  let controller: UnitTestingController;
+    let controller: UnitTestingController;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [UnitTestingController],
-    }).compile();
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            controllers: [UnitTestingController],
+            providers: [
+                {
+                    provide: UnitTestingService,
+                    useClass: UnitTestingService,
+                }
+            ]
+        }).compile();
 
-    controller = module.get<UnitTestingController>(UnitTestingController);
-  });
+        controller = module.get<UnitTestingController>(UnitTestingController);
+    });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
+    afterEach(() => {
+        mockApplyRule.mockClear();
+    });
+
+    it('should be defined', () => {
+        expect(controller).toBeDefined();
+    });
+
+    describe('#applyRule', () => {
+        it('should return status OK', () => {
+            mockApplyRule.mockImplementation(() => true);
+
+            expect(controller.applyRule('nice')).toEqual({"status": "ok"});
+        });
+
+        it('should throw BadRequest', () => {
+            mockApplyRule.mockImplementation(() => false );
+
+            expect(() => controller.applyRule('notnice')).toThrow(BadRequestException);
+        });
+    });
 });
